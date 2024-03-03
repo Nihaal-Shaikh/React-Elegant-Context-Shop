f1b1234ec709273edf33ada064d721ad8a55b89d
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-03-03 13:14:12 +0100

    Consuming the context

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
 import Product from './components/Product.jsx'
-import { CartContext } from './store/shoppin-cart-context.jsx';
+import { CartContext } from './store/shopping-cart-context.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -68,7 +68,7 @@ function App() {
   }
 
   return (
-    <CartContext.Provider>
+    <CartContext.Provider value={{ items: [] }}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
